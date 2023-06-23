33a0cebf9914836acecdbe9d0714ac702e0e231e
Pich Lyhong <lyhong.pich@gmail.com>
2023-06-23 23:52:28 +0700

    Update on login page

--- Book Store e commerce/src/views/LoginView.vue
+++ Book Store e commerce/src/views/LoginView.vue
@@ -21,10 +21,32 @@
         <Cart labelUrl="https://cdn-icons-png.flaticon.com/512/263/263142.png" labels="Cart(0)"></Cart>
     </header>
     <div class="flex h-[583] justify-center items-center">
-        <div class="flex justify-center items-center mt-[99px] mb-[97px] w-[664px] h-[583px] bg-cyan-500">
-            <div class="">
-                
+        <div class="flex flex-col items-center text-[32px] mt-[99px] mb-[97px] w-[664px] h-[583px] bg-sky-300">
+            <div class="mt-[70px]">
+                <span class="">Login</span>
             </div>
+            <div class="mt-[24px]">
+                <input type="text" class="w-[536px] h-[73px] placeholder:pl-[25px] rounded-[20px]" placeholder="Email">
+            </div>
+            <div class="mt-[19px]">
+                <input type="text" class="w-[536px] h-[73px] placeholder:pl-[25px] rounded-[20px]" placeholder="Password">
+                <img src="https://cdn-icons-png.flaticon.com/512/4254/4254118.png" alt="" class="w-[35px] h-[35px] mt-[-53px] ml-[475px] opacity-[35%]">
+            </div>
+            <div class="mt-[15px] pl-[350px] text-[24px]">
+                <span>Forget password?</span>
+            </div>
+            <div class="">
+                <button @click="$emit('logout')" class="w-[536px] h-[89px] bg-green-500 hover:bg-green-400">Sign In</button>
+            </div>
+            <div class="text-[24px] flex mt-[19px]">
+                <div class="">
+                    <span>Did't own an account yet?</span>
+                </div>
+                <div class="">
+                    <button class="underline">Register!</button>
+                </div>
+            </div>
+            
         </div>
     </div>
     
